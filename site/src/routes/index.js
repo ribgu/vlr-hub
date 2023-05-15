35d060895e399b758ff96aa18ccd60cd093dd0b1
Gustavo Ribeiro <125397719+ribgu@users.noreply.github.com>
2023-05-15 14:08:28 -0300

    att index.js

--- site/src/routes/index.js
+++ site/src/routes/index.js
@@ -1,8 +1,16 @@
 var express = require("express");
 var router = express.Router();
 
+// Essa variavel é utilizada para o script reconhecer caminhos no router.get
+var path = require("path");
+
+// Aqui ele envia para o GET o arquivo home.html no diretorio raiz
+// Troque o caminho em "" para o caminho da sua .html da Pagina Inicial
 router.get("/", function (req, res) {
-    res.render("index", { title: "Express" });
+    res.sendFile(path.join(__dirname, "../../public/pages/index.html"));
 });
+
+// Função para setar o diretorio raiz do home.html(no meu caso), assim ele conseguirá carregar o CSS e o JS das paginas
+router.use(express.static(path.join(__dirname, "../../public/pages")));
 
 module.exports = router;
