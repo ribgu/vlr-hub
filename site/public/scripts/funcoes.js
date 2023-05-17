7b34e3debc4f3e45f327fe3661336c9b8acdc787
ribgu <gustavo.ribeiro@sptech.school>
2023-05-17 16:04:02 -0300

    alteração css mapa

--- site/public/scripts/funcoes.js
+++ site/public/scripts/funcoes.js
@@ -5,15 +5,12 @@ function validarSessao() {
     var email = sessionStorage.EMAIL_USUARIO;
     var nome = sessionStorage.NOME_USUARIO;
 
-    var b_usuario = document.getElementById("b_usuario");
+    const botao_login = document.getElementById("botao_login");
 
     if (email != null && nome != null) {
-        // window.alert(`Seja bem-vindo, ${nome}!`);
-        b_usuario.innerHTML = nome;
+        botao_login.style.display = none
 
-        // finalizarAguardar();
-    } else {
-        window.location = "../login.html";
+        
     }
 }
 
